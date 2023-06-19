a0173f70f69a5ab893bf4d78ca43d291093d891c
Michel Eduardo <90931413+MichelEduardoBZ@users.noreply.github.com>
2023-06-19 08:56:05 -0300

    Adicionado relação entre usuário e autorização.

--- src/main/java/com/michel/dscommerce/entities/Product.java
+++ src/main/java/com/michel/dscommerce/entities/Product.java
@@ -23,10 +23,11 @@ public class Product {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
-@OneToMany(mappedBy = "id.product")
+    @OneToMany(mappedBy = "id.product")
     private Set<OrderItem> items = new HashSet<>();
 
-    public Product(){}
+    public Product() {
+    }
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -83,6 +84,7 @@ public class Product {
     public Set<OrderItem> getItems() {
         return items;
     }
+
     public List<Order> getOrders() {
         return items.stream().map(x -> x.getOrder()).toList();
     }
--- src/main/java/com/michel/dscommerce/entities/User.java
+++ src/main/java/com/michel/dscommerce/entities/User.java
@@ -3,7 +3,9 @@ package com.michel.dscommerce.entities;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_user")
@@ -20,6 +22,12 @@ public class User {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User(){}
 
@@ -82,5 +90,13 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
     }
 }
