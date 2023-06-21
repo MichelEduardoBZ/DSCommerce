3ff02acd27eff1765f9f08127ed488769a637451
Michel Eduardo <90931413+MichelEduardoBZ@users.noreply.github.com>
2023-06-21 15:53:16 -0300

    Implementando a paginação de produto sem descrição, ProductMinDTO

--- src/main/java/com/michel/dscommerce/config/AuthorizationServerConfig.java
+++ src/main/java/com/michel/dscommerce/config/AuthorizationServerConfig.java
@@ -55,7 +55,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-
         endpoints.authenticationManager(authenticationManager)
                 .tokenStore(tokenStore)
                 .accessTokenConverter(accessTokenConverter);
--- src/main/java/com/michel/dscommerce/services/ProductService.java
+++ src/main/java/com/michel/dscommerce/services/ProductService.java
@@ -1,7 +1,9 @@
 package com.michel.dscommerce.services;
 
+import com.michel.dscommerce.dto.CategoryDTO;
 import com.michel.dscommerce.dto.ProductDTO;
 import com.michel.dscommerce.dto.ProductMinDTO;
+import com.michel.dscommerce.entities.Category;
 import com.michel.dscommerce.entities.Product;
 import com.michel.dscommerce.repositories.ProductRepository;
 import com.michel.dscommerce.services.exceptions.DatabaseException;
@@ -66,14 +68,17 @@ public class ProductService {
         }
     }
 
-
     private void copyDtoToEntity(ProductDTO dto, Product entity) {
 
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
-
+        entity.getCategories().clear();
+        for (CategoryDTO catDTO : dto.getCategories()){
+            Category cat = new Category();
+            cat.setId(catDTO.getId());
+            entity.getCategories().add(cat);
+        }
     }
-
 }
